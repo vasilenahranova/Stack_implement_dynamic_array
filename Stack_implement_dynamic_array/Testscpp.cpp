63d777c1c5f125cdf51d4500f000c82e5e578aeb
vasilenahranova <97529297+vasilenahranova@users.noreply.github.com>
2022-10-26 15:26:07 +0300

    Add files via upload

---
+++ Stack_implement_dynamic_array/Testscpp.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include"Stack.h"
+#include"Doctest.h"
+
+TEST_CASE("Just after we create a stack it is empty") {
+    RStack<int> stack1;
+    CHECK(stack1.isEmpty()==1);
+}
+
+TEST_CASE("After pushing elements, it is no longer empty") {
+    RStack<int> stack1;
+    stack1.push(10);
+    CHECK(stack1.isEmpty()==0);
+}
+TEST_CASE("Testing the pop funtion") {
+    RStack<int> stack1;
+    stack1.push(10);
+    stack1.push(13);
+    CHECK(stack1.top() == 13);
+    stack1.pop();
+    CHECK(stack1.top() == 10);
+}
